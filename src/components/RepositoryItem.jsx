76f01993cf3869a9b22865bb51c5e97034dbad1c
marcelo.mileo <marcelo.mileo@nsb.com.br>
2022-03-18 15:39:57 -0300

    [IMPLEMENTAÇÂO] - Finalizado Chapter I React basico

--- src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem(props){
-    return(
-        <li>
-        <strong>{props.repository.name ?? 'default'}</strong>
-        <p>{props.repository.description ?? 'default'}</p>
-        <a href={props.repository.link}>Acessar repositório</a>
-    </li>
-    );
-}
